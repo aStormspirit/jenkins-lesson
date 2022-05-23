82d369360d18621f7c67e5a7706d062309f4ea24
Vladimir <Shinkarenkovova@gmail.com>
2022-05-23 20:45:44 +0300

    date

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+    <?php
+    //установка локали и выбор значения
+    setlocale(LC_ALL, "russian");
+    $day = strftime('%d');
+    $mon = strftime('%B');
+    $year = strftime('%Y');
+    ?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,5 +16,8 @@
 <body>
     <h1>Jenkins is awesome</h1>
     <h2>Learn php</h2>
+    <blockquote>
+        <?php echo "Сегодня ", $day, ' Число', $mon, ' Месяц ', $year, 'год'; ?>
+    </blockquote>
 </body>
 </html>
